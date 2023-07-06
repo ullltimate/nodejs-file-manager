f8b40ac90d84e0e6130df3965f1f11141e6fe543
ullltimate <ullltimate.zp@gmail.com>
2023-07-06 17:29:26 +0300

    feat: add command up and cd

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import process from 'node:process';
 import { stdin, stdout } from 'node:process';
 import { cwd } from 'node:process';
+import { homedir } from 'node:os';
 
 let userName;
 
@@ -26,15 +27,46 @@ const greeting = async () => {
 
 const linstenerConsole = async () => {
     stdout.write(`You are currently in ${cwd()}\n`);
+    stdout.write(`Please enter the command:\n`);
 
     stdin.on('data', data => {
-        
-        if (data.toString().trim() == 'exit'){
-            process.exit();
-        }
+        switch(data.toString().split(' ')[0].trim()){
+            case 'up':
+                up();
+                break;
+            case 'cd':
+                if(!data.toString().split(' ')[1].trim()){
+                    console.log('Please add correct command');
+                } else{
+                    cd(data.toString().split(' ')[1].trim());
+                }
+                break;
+            case 'exit':
+                process.exit();
+            default:
+                console.log('Please add correct command');
+            }
+        //if (data.toString().trim() === 'up'){
+        //    up();
+        //}
+        //if (data.toString().trim() === 'exit'){
+        //    process.exit();
+        //}
+        stdout.write(`You are currently in ${cwd()}\n`);
+        console.log();
     });
     process.on('SIGINT', () => process.exit());
     process.on('exit', () => stdout.write(`\nThank you for using File Manager, ${userName}, goodbye!\n`));
 }
 
+const up = () => {
+    process.chdir('../');
+}
+const cd = async (path) =>{
+    try {
+        process.chdir(path);
+    } catch (error) {
+        console.error(new Error(`Operation failed`));
+    }
+}
 await appFileManager();
